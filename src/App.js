b31360c00859db0685c622e2e0d8b898bdb01005
luoting <luoting@mcpayment.net>
2020-03-27 17:49:57 +0800

    引入antd，使用react-app-rewired修改默认配置，使用babel-plugin-import按需加载组件，使用less-loader自定义主题

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import { Button, message } from 'antd';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/**
+ * 应用根组件
+ */
+class App extends Component {
+
+    messageClick = () => {
+        message.success('click success.')
+    };
+
+    render() {
+        return <div className={'App'}>
+            <Button type="primary" onClick={ this.messageClick }>Button</Button>
+        </div>
+    }
 }
 
 export default App;
